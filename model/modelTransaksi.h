10d670441fdefd231a536c379f81a1d724c263e2
Aceells <xaseyocl.146@gmail.com>
2024-01-14 18:07:38 +0700

    Update 14 Jan 2024

--- model/modelTransaksi.h
+++ model/modelTransaksi.h
@@ -31,3 +31,93 @@ void mTransaksiBus(string inpNamaBis, string inpStartKeberangkatan, string inpTu
         cout << "Maaf, tiket yang Anda cari tidak ditemukan.\n";
     }
 }
+
+// void mTransaksiPesawat(string inpNamaPesawat, string inpStartKeberangkatan, string inpTujuanKeberangkatan)
+// {
+//     int indexPesawat = mSearchJdwlPesawat(inpNamaPesawat, inpStartKeberangkatan, inpTujuanKeberangkatan);
+//     if (indexPesawat != -1)
+//     {
+//         cout << "Informasi Tiket yang anda cari:\n";
+//         cout << "Nama Pesawat :" << namaPesawat[indexPesawat] << endl;
+//         cout << "Stasiun pertama :" << awalKeberangkatan[indexPesawat] << endl;
+//         cout << "Rute Tujuan Ke :" << tujuanKeberangkatan[indexPesawat] << endl;
+//         // Menampilkan semua jadwal pesawat
+//         cout << "Apakah Anda setuju dengan hasil data yang ditemukan? (y/n): ";
+//         char konfirmasi;
+//         cin >> konfirmasi;
+//         if (konfirmasi == 'y' || konfirmasi == 'Y')
+//         {
+//             cout << "Berapa tiket yang ingin Anda beli? ";
+//             int jumlahTiket;
+//             cin >> jumlahTiket;
+//             // Proses pembelian tiket
+//         }
+//     }
+//     else
+//     {
+//         cout << "Maaf, tiket yang Anda cari tidak ditemukan.\n";
+//     }
+// }
+
+// void mTransaksiKereta(string inpNamaKereta, string inpStartKeberangkatan, string inpTujuanKeberangkatan)
+// {
+//     int indexKereta = mSearchJdwlKereta(inpNamaKereta, inpStartKeberangkatan, inpTujuanKeberangkatan);
+//     if (indexKereta != -1)
+//     {
+//         cout << "Informasi Tiket yang anda cari:\n";
+//         cout << "Nama Kereta :" << namaKereta[indexKereta] << endl;
+//         cout << "Stasiun pertama :" << awalKeberangkatan[indexKereta] << endl;
+//         cout << "Rute Tujuan Ke :" << tujuanKeberangkatan[indexKereta] << endl;
+//         // Menampilkan semua jadwal kereta
+//         cout << "Apakah Anda setuju dengan hasil data yang ditemukan? (y/n): ";
+//         char konfirmasi;
+//         cin >> konfirmasi;
+//         if (konfirmasi == 'y' || konfirmasi == 'Y')
+//         {
+//             cout << "Berapa tiket yang ingin Anda beli? ";
+//             int jumlahTiket;
+//             cin >> jumlahTiket;
+//             // Proses pembelian tiket
+//         }
+//     }
+//     else
+//     {
+//         cout << "Maaf, tiket yang Anda cari tidak ditemukan.\n";
+//     }
+// }
+
+void vTransaksiBus()
+{
+    string namaBis, startKeberangkatan, tujuanKeberangkatan;
+    cout << "Masukan nama bis yang anda cari : ";
+    cin >> namaBis;
+    cout << "Masukan stasiun pertama keberangkatan : ";
+    cin >> startKeberangkatan;
+    cout << "Masukan tujuan keberangkatan : ";
+    cin >> tujuanKeberangkatan;
+    mTransaksiBus(namaBis, startKeberangkatan, tujuanKeberangkatan);
+}
+
+// void vTransaksiPesawat()
+// {
+//     string namaPesawat, startKeberangkatan, tujuanKeberangkatan;
+//     cout << "Masukan nama pesawat yang anda cari : ";
+//     cin >> namaPesawat;
+//     cout << "Masukan stasiun pertama keberangkatan : ";
+//     cin >> startKeberangkatan;
+//     cout << "Masukan tujuan keberangkatan : ";
+//     cin >> tujuanKeberangkatan;
+//     // mTransaksiPesawat(namaPesawat, startKeberangkatan, tujuanKeberangkatan);
+// }
+
+// void vTransaksiKereta()
+// {
+//     string namaKereta, startKeberangkatan, tujuanKeberangkatan;
+//     cout << "Masukan nama kereta yang anda cari : ";
+//     cin >> namaKereta;
+//     cout << "Masukan stasiun pertama keberangkatan : ";
+//     cin >> startKeberangkatan;
+//     cout << "Masukan tujuan keberangkatan : ";
+//     cin >> tujuanKeberangkatan;
+//     // mTransaksiKereta(namaKereta, startKeberangkatan, tujuanKeberangkatan);
+// }
